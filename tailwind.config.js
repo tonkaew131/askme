9a69aaebfead5405440046d477cd119cc35a6c49
tonkaew131 <nongtonkaew@gmail.com>
2022-08-14 13:49:15 +0700

    Added Color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                base: '#A1C3D1',
+                purple: '#B39BC8',
+                text: '#F0EBF4',
+                secondary: '#F172A1',
+                primary : '#E64398',
+            }
+        },
     },
     plugins: [],
 }
