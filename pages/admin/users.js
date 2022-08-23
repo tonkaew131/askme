3c4783ea49e635d611496712022dd1878615e1f4
tonkaew131 <nongtonkaew@gmail.com>
2022-08-23 21:42:29 +0700

    Added search menu in admin user manager page (/admin/user)

--- pages/admin/users.js
+++ pages/admin/users.js
@@ -4,6 +4,30 @@ import React, { useEffect, useState } from 'react';
 import NavbarAdmin from '../../components/NavbarAdmin';
 import LoadingSpinner from '../../components/LoadingSpinner';
 
+function SearchMenu(props) {
+    return (
+        <div className="text-black font-Prompt mx-4 mt-4">
+            <p className="text-3xl font-semibold">Manage users</p>
+
+            <div className="mt-3 sm:flex sm:mt-5">
+                <div className="sm:flex">
+                    <p className="mb-1 sm:my-auto sm:mr-3">Search: </p>
+                    <input placeholder={`${props.count} users...`} className="bg-white shadow py-2 px-3 rounded focus:outline-none w-full"></input>
+                </div>
+                <div className="sm:flex mt-1 sm:mt-0 sm:ml-5">
+                    <p className="mb-1 sm:my-auto sm:mr-3">Roles: </p>
+                    <select name="roles" id="roles" className="bg-white shadow py-2 px-3 rounded focus:outline-none">
+                        <option value="ADMIN">Admin</option>
+                        <option value="USER" selected="selected">User</option>
+                        <option value="REVORKED">Revorked</option>
+                    </select>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
 export default withPageAuthRequired(function User({ user }) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
@@ -24,7 +48,7 @@ export default withPageAuthRequired(function User({ user }) {
             }
 
             if ('error' in json) {
-                setError(json.error.message);
+                return setError(json.error.message);
             }
 
             console.log(json); // For Error
@@ -39,15 +63,15 @@ export default withPageAuthRequired(function User({ user }) {
         </div>
     );
 
-    if (error) return 'Unauthorized';
+    if (error) return error;
 
     return (
-        <div className='bg-white w-screen h-screen'>
+        <div className='bg-gray-100 w-screen h-screen'>
             <NavbarAdmin />
 
-            <div className='text-black font-Montserrat font-bold text-3xl'>
-                <p>Users</p>
-            </div>
+            <SearchMenu
+                count={userCount}
+            />
         </div>
     );
 });
