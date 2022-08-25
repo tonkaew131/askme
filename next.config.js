85ccf2b6e04d7788cdeefe86963bc701e79bf4d3
tonkaew131 <nongtonkaew@gmail.com>
2022-08-25 23:30:37 +0700

    Added redirect for login & logout pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    async redirects() {
+        return [
+            {
+                source: '/account/login',
+                destination: '/api/auth/login',
+                permanent: true,
+            },
+            {
+                source: '/account/logout',
+                destination: '/api/auth/logout',
+                permanent: true,
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
