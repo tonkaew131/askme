a22002e5242a555e7f6b6da48e2ab76ed8804b13
Athicha L. (mac) <nongtonkaew@gmail.com>
2022-08-26 16:17:14 +0700

    Added shared email validator function

---
+++ shared/utils.js
@@ -0,0 +1,7 @@
+module.exports = {
+    emailValidator: function (email) {
+        const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (email.match(emailRegex)) return true;
+        return false;
+    }
+}
