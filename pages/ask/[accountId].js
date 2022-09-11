8f37d6ec404cba0fd1b6137f725db1a26d0766e2
tonkaew131 <nongtonkaew@gmail.com>
2022-09-11 21:58:11 +0700

    Add api call to send answer button

--- pages/ask/[accountId].js
+++ pages/ask/[accountId].js
@@ -8,6 +8,7 @@ export default function AskMe() {
     const { accountId } = router.query;
 
     const [question, setQuestion] = useState('');
+    const [answer, setAnswer] = useState('');
 
     useEffect(() => {
         if (!accountId) return;
@@ -26,6 +27,26 @@ export default function AskMe() {
         fetchData().catch(error => console.error(error));
     }, [accountId]);
 
+    function handleAnswerChange(text) {
+        return setAnswer(text);
+    }
+
+    function handleSendAnswer() {
+        if (answer == '') {
+            return;
+        }
+
+        const sendData = async () => {
+            var data = await fetch(`/api/question/${accountId}?text=${answer}`, {
+                method: 'POST'
+            });
+            var json = await data.json();
+            console.log(json); // For Error
+        };
+
+        sendData().catch(error => console.error(error));
+    }
+
     return (
         <div className="bg-slate-800 w-screen h-screen font-Prompt text-4xl flex justify-center">
             <Head>
@@ -38,13 +59,18 @@ export default function AskMe() {
                 <div className="bg-slate-500 w-10/12 pb-10 overflow-clip rounded-3xl mx-auto text-center text-2xl shadow-2xl shadow-slate-700 lg:w-1/3">
                     <p className="bg-text py-2 text-black font-extrabold">@{accountId}</p>
                     <p className="w-10/12 mx-auto mt-4">{question}</p>
-                    <div className="mt-4 w-3/4 bg-white bg-opacity-60 mx-auto p-1 rounded-full">
-                        <input className="px-2 w-full bg-transparent text-black focus:outline-none"></input>
+
+                    {/* Input */}
+                    <div className="mt-4 w-3/4 bg-white bg-opacity-60 mx-auto p-1 rounded-full ">
+                        <input className="px-2 w-full bg-transparent text-black focus:outline-none" onChange={(e) => handleAnswerChange(e.target.value)}></input>
                     </div>
                 </div>
 
-                <div className="bg-text text-slate-800 w-1/2 text-center py-3 rounded-full mx-auto mt-[25vh] text-3xl shadow-2xl shadow-slate-700 font-bold active:scale-[0.98] lg:w-2/12">
-                    ส่ง
+                {/* Send Button */}
+                <div
+                    className="bg-text text-slate-800 select-none w-1/2 text-center py-3 rounded-full mx-auto mt-[25vh] text-3xl shadow-2xl shadow-slate-700 font-bold active:scale-[0.98] lg:w-2/12 hover:cursor-pointer"
+                    onClick={(e) => handleSendAnswer()}
+                >ส่ง!
                 </div>
             </div>
         </div>
